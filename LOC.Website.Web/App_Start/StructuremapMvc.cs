b0c30941b9909bdd72934049f578e1004d7e2933
timing1337 <timing@crepe.moe>
2024-02-28 22:38:04 +0700

    fix fix

--- LOC.Website.Web/App_Start/StructuremapMvc.cs
+++ LOC.Website.Web/App_Start/StructuremapMvc.cs
@@ -1,6 +1,6 @@
 using LOC.Website.Web.App_Start;
 
-[assembly: WebActivator.PreApplicationStartMethod(typeof(StructuremapMvc), "Start")]
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(StructuremapMvc), "Start")]
 
 namespace LOC.Website.Web.App_Start {
     using System.Web.Mvc;
